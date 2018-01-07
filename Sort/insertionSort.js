c96a2ca42c2d3cc5cc61999a16dff0558b717859
Jessie Hsieh <jessie75919@gmail.com>
2018-01-07 11:55:08 +0800

    add obj sort

--- Sort/insertionSort.js
+++ Sort/insertionSort.js
@@ -1,10 +1,39 @@
 // var num = [3, 2, 1, 5, 8, 9, 6, 4];
 var num = [33,2,11,35,58,93,62,42];
-console.log(num);
 
-insertionSort(num);
+var persons = [
+    {
+        "name": "john",
+        "age" : "23"
+    },
+    {
+        "name": "harry",
+        "age" : "41"
+    },
+    {
+        "name": "jack",
+        "age" : "35"
+    },
+    {
+        "name": "ali",
+        "age" : "15"
+    },
+    {
+        "name": "juc",
+        "age" : "5"
+    },
+    {
+        "name": "Jessie",
+        "age" : "30"
+    },
+    {
+        "name": "Joe",
+        "age" : "85"
+    }
+];
 
-function insertionSort(num) {
+
+function insertionSortAsc(num) {
     for (var i = 0; i < num.length; i++) {
         for (var j = 1; j <= i; j++) {
             var rightIndex = 0;
@@ -26,4 +55,52 @@ function insertionSort(num) {
         // console.log('inside', num);
     }
 }
-console.log(num);
+// insertionSortAsc(num);
+// console.log(num);
+
+function insertionSortDesc(num) {
+    for (var i = 0; i < num.length; i++) {
+        for (var j = 1; j <= i; j++) {
+            var rightIndex = 0;
+            var temp = 0;
+            // search the number bigger than me
+            rightIndex = i - j
+            // console.log("i = " + i);
+            if (num[rightIndex] <= num[rightIndex+1]) {
+                temp = num[rightIndex+1];
+                num[rightIndex+1] = num[rightIndex];
+                num[rightIndex] = temp;
+            }
+            // search the number smaller than me
+            else{
+                break;
+            }
+            // console.log(num);
+        }
+        // console.log('inside', num);
+    }
+}
+
+// insertionSortDesc(num);
+// console.log(num);
+
+function personInsertionSortAsc(num) {
+    for (var i = 0; i < num.length; i++) {
+        for (var j = 1; j <= i; j++) {
+            var rightIndex = 0;
+            var temp = 0;
+            rightIndex = i - j
+            if (parseInt(num[rightIndex].age) >= parseInt(num[rightIndex+1].age)) {
+                temp = num[rightIndex+1];
+                num[rightIndex+1] = num[rightIndex];
+                num[rightIndex] = temp;
+            }
+            else break;
+        }
+    }
+}
+
+console.log(persons);
+personInsertionSortAsc(persons);
+console.log(persons);
+
